47b1514ca01e0f60eb1826235c6d6863e8a4767a
Mikita Kukavenka <godinfrog@gmail.com>
2023-05-03 12:52:08 +0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import packageJson from '../package.json';
 import { Logger, Module, OnModuleInit } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { TgBotService } from './service/tg-bot.service';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -19,7 +19,23 @@ import { PollService } from './service/poll.service';
 export class AppModule implements OnModuleInit {
   private readonly logger = new Logger('AppModule');
 
+  constructor(private configService: ConfigService) {}
+
   onModuleInit() {
     this.logger.log(`AppModule started v${packageJson.version}.`);
+    this.logger.log(`Configuration:`);
+    this.logger.log(
+      `\t gameChatId : ${this.configService.get('bot.gameChatId')}`,
+    );
+    this.logger.log(
+      `\t gameChatPollThreadId : ${this.configService.get(
+        'bot.gameChatPollThreadId',
+      )}`,
+    );
+    this.logger.log(
+      `\t listenToAllMessages : ${this.configService.get(
+        'bot.listenToAllMessages',
+      )}`,
+    );
   }
 }
